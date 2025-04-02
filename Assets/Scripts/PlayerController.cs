be658bbe73ffe60e4b142b7fcbe182c008079d23
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2025-04-02 12:38:44 +0700

    Теперь он может поворачивать и ты им управляешь

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
+    public float turnSpeed;
+
+    public float horInput;
+    public float vertInput;
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horInput = Input.GetAxis("Horizontal");
+        vertInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horInput);
     }
 }
